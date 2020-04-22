1a7210e3143127950d22dbc26130678c51fe2326
konemshad <konemshad@gmail.com>
2020-04-22 13:08:46 +0530

    changed a dependency and cleaned some code

--- ml/graph.py
+++ ml/graph.py
@@ -3,6 +3,8 @@ from .preprocess.util import Flatten
 from .optimizers import optimizer
 from ml import losses
 import numpy as np	
+
+optimizers_dict={"sgd":optimizer.Optimizer,"SGD":optimizer.Optimizer}
 class Graph():
 	def __init__(self,flatten=True):
 		self.in_flag=flatten
@@ -13,6 +15,7 @@ class Graph():
 		else:
 			if not self.history:self.history.append(ly)
 			else:
+				self.with_bias=ly.set_bias
 				ly.set_weights(self.history[-1].get_units)
 				self.history.append(ly)
 	@property
@@ -20,13 +23,18 @@ class Graph():
 		print("\t\tNetwork Graph:\n")
 		for i in self.history:
 			print(i.__class__.__name__,"\tunits:%d\tactivation:%s\n"%(i.get_units,i.get_activation.__name__))
-	def train(self,X=None,Y=None,lr=1e-3,epochs=10):
+	def train(self,X=None,Y=None,lr=0.1,epochs=10,optimizer=None):
+		if not optimizer:
+			raise InputError("expected an optimizer")
+		if isinstance(optimizer,str):
+			try:
+				op=optimizers_dict[optimizer]
+				self.opt=op(X,Y,lr,epochs,self.history)
+			except:
+				raise InputError("invalid optimizer provided")
 		if self.in_flag:
 			X=Flatten(X)
-		print("training_shape:",X.shape)
-		Graph.opt=optimizer.Optimizer(X=X,Y=Y,lr_rate=lr,epochs=epochs)
-		self.opt
-		Graph.__dict__["opt"].optimize()
+		self.opt.optimize()
 	def next_layer(self,l):return self.history[self.history.index(l)+1]
 	def prev_layer(self,l):return self.history[self.history.index(l)-1]
 	def predict(self,X):
--- ml/optimizers/optimizer.py
+++ ml/optimizers/optimizer.py
@@ -4,7 +4,7 @@ from ml import epoch_an
 from ml.activation import activations
 from ml.layer import layer
 class Optimizer:
-	def __init__(self,X,Y,lr_rate=1e-3,epochs=10,batch_size=16,loss="cost"):
+	def __init__(self,X,Y,lr_rate=1e-3,epochs=10,layers=None,batch_size=32,loss="cost"):
 		self.X=X
 		self.Y=Y
 		self.lr=lr_rate
@@ -12,6 +12,8 @@ class Optimizer:
 		self.batch_size=batch_size
 		self.data=np.asarray(list(zip(X,Y)))
 		self.loss=loss
+		self.graph_layers=layers
+		self.with_bias=self.graph_layers[-1].set_bias
 	def make_batches(self,batch_size):
 		"""makes batches
 
@@ -26,27 +28,35 @@ class Optimizer:
 			ite=q(ite)
 			if ite==(len(self.data),len(self.data)):break
 			else:size_index_list.append(ite)
-		self.batches=[self.data[_[0]:_[1]] for _ in size_index_list]
+		self.batches=np.array([self.data[_[0]:_[1]] for _ in size_index_list])
+		np.random.shuffle(self.batches)
 	def optimize(self,cost="cost"):
 		self.count=0
 		self.make_batches(self.batch_size)
 		self.loss=losses.cost
 		for i in range(self.epochs):
 			print("\t\t\t\trunning epoch:%d"%(i+1))
-			#epoch_an.a(9209)
-			for i in self.batches:self.update_on_batch(i,batch_size=self.batch_size)
+			for j in self.batches:
+				loss=self.update_on_batch(j,batch_size=self.batch_size)
+				#print("loss after a batch ",np.sum(loss))
 	def update_on_batch(self,batch,batch_size=16):
 		trainable_layers=self.graph_layers[1:]
+		batch_loss=0
 		batch_grads_w=[np.zeros(i.get_weights.shape) for i in trainable_layers]
-		batch_grads_b=[np.zeros(i.get_biases.shape) for i in trainable_layers]
+		if self.with_bias:
+			batch_grads_b=[np.zeros(i.get_biases.shape) for i in trainable_layers]
 		for ex in batch:
 			self.count+=1
 			single_grads,cpl,loss=self.update_on_single_example(ex[0],ex[1])
+			batch_loss+=loss
 			batch_grads_w=[a+b for a,b in zip(batch_grads_w,single_grads)]
-			batch_grads_b=[a+b for a,b in zip(batch_grads_b,cpl)]
+			if self.with_bias:
+				batch_grads_b=[a+b for a,b in zip(batch_grads_b,cpl)]
 		for i in range(len(trainable_layers)):
 			trainable_layers[i].weights-=(self.lr/batch_size)*batch_grads_w[i]
-			trainable_layers[i].biases-=(self.lr/batch_size)*batch_grads_b[i]
+			if self.with_bias:
+				trainable_layers[i].biases-=(self.lr/batch_size)*batch_grads_b[i]
+		return batch_loss
 	def update_on_single_example(self,X,Y):
 		gradient_updates_w=[]
 		gradient_updates_b=[]
@@ -55,35 +65,30 @@ class Optimizer:
 		trainable_layers=self.graph_layers[1:]
 		for i in trainable_layers:
 			pred=i(pred)
+		print("predicted",np.argmax(pred))
 		network_loss=losses.cost(pred,Y)
+		print("network_loss:",network_loss)
 		trainable_layers.reverse()
 		rev=trainable_layers
 		for i in range(len(rev)):
 				if not costs_per_layer:
 					loss=(network_loss)*(rev[i].get_activation(rev[i].get_weighted_sum,prime=True))
-					grad=np.dot(loss,self.graph_i.prev_layer(rev[i]).get_activations.T)
+					grad=np.dot(loss,rev[1].get_activations.T)
+					print("grad1:",grad)
 					gradient_updates_w.append(grad)
 					gradient_updates_b.append(loss)
 					rev[i].loss=loss
 					costs_per_layer.append(loss)	
 				else:
-					res=np.dot(self.graph_i.next_layer(rev[i]).get_weights.T,self.graph_i.next_layer(rev[i]).loss)
-					loss=np.dot(self.graph_i.next_layer(rev[i]).get_weights.T,self.graph_i.next_layer(rev[i]).loss)*(rev[i].get_activation(rev[i].get_weighted_sum,prime=True))
+					loss=np.dot(rev[i-1].get_weights.T,rev[i-1].loss)*(rev[i].get_activation(rev[i].get_weighted_sum,prime=True))
+					print("grad2:",np.sum(loss))
 					costs_per_layer.append(loss)
 					rev[i].loss=loss
 					gradient_updates_b.append(loss)
-					if isinstance(self.graph_i.prev_layer(rev[i]),layer.Input):
-						grad=grad=np.dot(loss,X.T)
+					if isinstance(self.graph_layers[-(i+2)],layer.Input):
+						grad=np.dot(loss,X.T)
 					else:
-						grad=np.dot(loss,self.graph_i.prev_layer(rev[i]).get_activations.T)
+						grad=np.dot(loss,rev[i+1].get_activations.T)
 					gradient_updates_w.append(grad)
+		# if self.with_bias:
 		return  reversed(gradient_updates_w),reversed(costs_per_layer),network_loss
-
-
-
-
-
-	def __get__(self,g_inst,o):
-		self.graph_i=g_inst
-		if not g_inst==None:
-			self.graph_layers=self.graph_i.__dict__["history"]
